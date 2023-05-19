90a45d8d8714d1329228ca43413a9f94d3ead7d6
Molmin <115549630@qq.com>
2023-05-19 13:46:24 +0800

    allow to set commit message

--- src/bin/publish.js
+++ src/bin/publish.js
@@ -1,12 +1,20 @@
 const {spawnSync} = require('child_process');
 const ghpage = require('gh-pages');
+const {existsSync, mkdirSync, readFileSync, writeFileSync} = require('fs')
+
+if(!existsSync('tmp'))
+    mkdirSync('tmp');
+writeFileSync('tmp/commit_message','');
+spawnSync('gedit',['tmp/commit_message']);
 
 spawnSync('rm',['-rf','node_modules/gh-pages/.cache']);
 spawnSync('rm',['-rf','node_modules/.cache/gh-pages']);
 
+console.log(`cache cleared`)
+
 ghpage.publish('dist',{
     branch: 'master',
-    message: 'Updates',
+    message: readFileSync('tmp/commit_message','utf8'),
     repo: 'git@github.com:OIDB/BZOJ-statements.git'
 },err=>{
     if(err)console.log(err.message);
